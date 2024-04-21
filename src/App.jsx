45a792bc1b28a4f26087f167641aa7e9d859d6fd
shjnd <kishandudhat2315@gmail.com>
2024-04-21 21:15:50 +0530

    basic css are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <p>App</p>
-    </>
+    <div className='App' >
+      <h1 className='title'>Hello world !!!</h1>
+      <div className='box'>
+        <h2>This is a box</h2>
+      </div>
+    </div>
   )
 }
 
