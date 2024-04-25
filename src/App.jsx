653032e5038cdd1b108b5e82e4187ebc64ada466
shjnd <kishandudhat2315@gmail.com>
2024-04-25 22:29:30 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { Toggle } from './components/Toggle/Toggle';
+import useLocalStorage from 'use-local-storage';
 
 function App() {
+  // const [isDark,setIsDark] = useState(false);
+  // const [isDark,setIsDark] = useLocalStorage('isDark',false);
+
+  const preferences = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [isDark,setIsDark] = useLocalStorage('isDark',preferences);
 
   return (
-    <div className='App' >
+    <div className='App' data-theme={isDark ? 'dark' : 'light'}>
+      <Toggle
+        isChecked={isDark}
+        handleChange={() => setIsDark(!isDark)}
+      />
       <h1 className='title'>Hello world !!!</h1>
       <div className='box'>
         <h2>This is a box</h2>
